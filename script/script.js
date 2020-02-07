f0a6b7c43097923887d26195f80a9edc9e360db8
KidPaddle0 <biskupskiandrzej@gmial.com>
2020-02-07 13:57:17 +0100

    Zaawansowane prace nad sekcją kontakty.

--- script/script.js
+++ script/script.js
@@ -6,10 +6,11 @@ window.addEventListener('load', function(e){
     pos_3 = bg - layer03.clientHeight + 'px';				// jak to się mówi 'wcześniej mi działało'
     layer03.style.marginTop= pos_3;
 
+//obrazek tytuowy pierwszej sekcji
     const bg_img_first_section = document.querySelector('.first_img');
     pos_4 = bg + 'px';
     bg_img_first_section.style.marginTop= pos_4;
-
+//napis tytuowy drugiej sekji
     const title_img_first_section = document.querySelector('.first_title');
     pos_5 = bg + bg_img_first_section.clientHeight /2 + 'px';
     title_img_first_section.style.marginTop = pos_5;
@@ -22,19 +23,46 @@ window.addEventListener('load', function(e){
     let h = bg_img_first_section.clientHeight;
     let th = h + bg -50 + 'px';
     fs.style.marginTop = th;
+
 //obrazek tytułowy drugiej sekcji
     const bg_img_second_section = document.querySelector('.bg_image_s2');
     let pos_img_s2 = bg + fs.clientHeight + bg_img_first_section.clientHeight + 'px';
     bg_img_second_section.style.marginTop = pos_img_s2;
-
+//napis tytułowy drugiej sekcji
     const title_img_second_section = document.querySelector('.title_s2');
     posX_title_s2 = bg_img_second_section.clientWidth / 2 - 184 + 'px';
     posY_title_s2 = bg + fs.clientHeight + bg_img_first_section.clientHeight + bg_img_second_section.clientHeight + 'px';
     title_img_second_section.style.marginLeft= posX_title_s2;
     title_img_second_section.style.marginTop = posY_title_s2;
+//terść drugiej sekcji
+    const ss = document.querySelector('.ss_content');
+    let ss_h = bg_img_second_section.clientHeight;
+    let ss_th = ss_h + bg + bg_img_first_section.clientHeight + fs.clientHeight -
+    100 + 'px';
+    ss.style.marginTop = ss_th;
 
+//motto
+    const motto = document.querySelector('.motto');
+    let m_h = ss_h + bg + bg_img_first_section.clientHeight + fs.clientHeight + ss.clientHeight + 'px';
+    motto.style.marginTop = m_h;
 
+const motto2 = document.querySelector('.motto2');
 
+    const title_motto = document.querySelector('.motto_title');
+    let m_Y = motto2.clientHeight;
+    let m_X = motto2.clientWidth;
+    let mt_pos_Y = m_Y/2 - title_motto.clientHeight/2 + 'px';
+    let mt_pos_X = m_X/2 - title_motto.clientWidth/2 + 'px';
+    title_motto.style.marginTop = mt_pos_Y;
+    title_motto.style.marginLeft = mt_pos_X;
+
+    const kontakt = document.querySelector('.kontakt');
+    let kon_pos = ss_h + bg + bg_img_first_section.clientHeight + fs.clientHeight + ss.clientHeight /*+ motto2.clientHeight*/ + bg_img_first_section.clientHeight + 'px';
+    //let kon_pos = motto.getBoundingClientRect();
+    //let pos_k = kon_pos.bottom + 'px';
+    //console.log(pos_k);
+    kontakt.style.marginTop = kon_pos;
+i
 });
 
 window.addEventListener('resize', function(e){
@@ -79,6 +107,23 @@ window.addEventListener('resize', function(e){
     posY_title_s2 = posY_title_s2 = bg_height + fs.clientHeight + bg_img_first_section.clientHeight + bg_img_second_section.clientHeight + 'px';
     title_img_second_section.style.marginLeft= posX_title_s2;
     title_img_second_section.style.marginTop = posY_title_s2;
+
+    const ss = document.querySelector('.ss_content');
+    let ss_h = bg_img_second_section.clientHeight;
+    let ss_th = bg_height + bg_img_firs_section_height + fs.clientHeight + ss_h -200+'px';
+    ss.style.marginTop = ss_th;
+
+    const motto = document.querySelector('.motto');
+    let m_h = bg_height +  bg_img_firs_section_height + h + ss_h + ss.clientHeight + 'px';
+    motto.style.marginTop = m_h;
+    
+    const title_motto = document.querySelector('.motto_title');
+    let m_Y = motto.clientHeight;
+    let m_X = motto.clientWidth;
+    let mt_pos_Y = m_Y/2 + title_motto.clientHeight/2 + 'px';
+    let mt_pos_X = m_X/2 - title_motto.clientWidth/2 + 'px';
+    title_motto.style.marginTop = mt_pos_Y;
+    title_motto.style.marginLeft = mt_pos_X;
 });
 
 window.addEventListener('scroll', function(e) {
